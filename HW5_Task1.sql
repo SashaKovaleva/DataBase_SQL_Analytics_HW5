631a682679cef0df64b33309d8f67b18fb2f06b8
GalinaKovalevaLaptop <7334153@gmail.com>
2024-03-23 16:12:06 +0300

    added tasks solutions

---
+++ HW5_Task1.sql
@@ -0,0 +1,11 @@
+-- Вы работаете с MySQL
+-- Введите свой код ниже
+SELECT 
+  DENSE_RANK() OVER(ORDER BY COUNT(m.id) DESC) as rank_message,
+  u.firstname,
+  u.lastname,
+  COUNT(m.id) as cnt
+FROM users u
+LEFT JOIN messages m ON m.from_user_id = u.id
+GROUP BY u.id
+ORDER BY rank_message
---
+++ HW5_Task2.sql
@@ -0,0 +1,9 @@
+-- Вы работаете с MySQL
+-- Введите свой код ниже
+SELECT
+    id,
+    created_at,
+    LEAD(created_at) OVER (ORDER BY created_at) as lead_time,
+    TIMESTAMPDIFF(MINUTE,created_at,(LEAD(created_at) OVER (ORDER BY created_at))) as minute_lead_diff
+FROM messages
+ORDER BY created_at
